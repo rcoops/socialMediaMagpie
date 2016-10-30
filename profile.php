3d063b5f2896856db12684fa88696af3e89e9dbd
TTr2 <tangoindigomacho@gmail.com>
2016-10-30 08:28:04 +0000

    Fantastic webz

---
+++ profile.php
@@ -0,0 +1,11 @@
+<?php require('./Views/template/header.phtml');
+
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 30/10/2016
+ * Time: 03:19
+ */
+
+require_once('./Views/profile.phtml');
+require_once('./Views/template/footer.phtml');
