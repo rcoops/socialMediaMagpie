05c9cb66925d04d199806ed8770a2911e6c6984c
Chris Dromey <cdromey@sis.tv>
2016-10-30 01:43:32 +0100

    index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'models\MoviesTable.php';
-require_once 'models\Member.php';
+//require_once 'models\MoviesTable.php';
+//require_once 'models\Member.php';
 
 /**
  * Home page of website, displays Bootstrap carousel of newest movies in database.
@@ -9,22 +9,22 @@ require_once 'models\Member.php';
  */
 
 session_start();
-$movies_table = new MoviesTable();
+//$movies_table = new MoviesTable();
 
-if (!isset($_SESSION['basket'])){
-    $_SESSION['basket'] = array();
-}
-$view = new stdClass();
-$view->pageTitle = 'Homepage - TTDVD';
-
-$view->carousel = $movies_table->filter_latest_movies();
-usort($view->carousel, "cmp_year");
-
-/* The SQL query already sorts Movie objects by year of release in descending order but the
- comparator assigns a new index in order from zero, required for carousel identifier features. */
-function cmp_year($a, $b)
-{
-    return strcmp($b->getYear(), $a->getYear());
-}
+//if (!isset($_SESSION['basket'])){
+//    $_SESSION['basket'] = array();
+//}
+//$view = new stdClass();
+//$view->pageTitle = 'Homepage - TTDVD';
+//
+//$view->carousel = $movies_table->filter_latest_movies();
+//usort($view->carousel, "cmp_year");
+//
+///* The SQL query already sorts Movie objects by year of release in descending order but the
+// comparator assigns a new index in order from zero, required for carousel identifier features. */
+//function cmp_year($a, $b)
+//{
+//    return strcmp($b->getYear(), $a->getYear());
+//}
 
 require_once('Views/index.phtml');
